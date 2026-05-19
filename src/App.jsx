b258dfe2deeab2ed1264560d8eeaf01097bf0062
ShanujPatel <63713962+ShanujPatel@users.noreply.github.com>
2026-05-19 15:24:27 +0100

    Add stats dashboard section below countdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Countdown from './Countdown'
+import Stats from './Stats'
 
 export default function App() {
-  const start = new Date(2026, 4, 19, 0, 0, 0)
+  const start  = new Date(2026, 4, 19, 0, 0, 0)
   const target = new Date(2026, 7, 27, 0, 0, 0)
 
   return (
     <div className="app">
-      <h1>Countdown to 27/08/2026</h1>
-      <Countdown startDate={start} targetDate={target} />
+      <section className="hero">
+        <h1>Countdown to 27/08/2026</h1>
+        <Countdown startDate={start} targetDate={target} />
+      </section>
+      <Stats startDate={start} targetDate={target} />
     </div>
   )
 }
